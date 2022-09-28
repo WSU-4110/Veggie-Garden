17a54901f4640aa7c60daf6e7315fd42dc66aa1f
Nicholas Arsenault <nickarsenault18@gmail.com>
2022-09-27 22:17:47 -0400

    Button rerouting is working, Login moves to home screen, create account moves to account creation screen. New text boxes and button in Create Account, new toolbar and text in Home Page.

--- App/app/src/main/java/com/example/app/Title_Page.java
+++ App/app/src/main/java/com/example/app/Title_Page.java
@@ -2,7 +2,9 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Title_Page extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Title_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_page);
+    }
+// this gets a button press to move pages, add: android:onClick="moveToTitle" to xml file under the button's area, and add: tools:ignore="UsingOnClickInXml" to clear the error attached.
+    public void moveToHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void moveToCreationPage(View view) {
+        Intent intent = new Intent(this, Create_Account_Page.class);
+        startActivity(intent);
     }
 }
