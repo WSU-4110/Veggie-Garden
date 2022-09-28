6b4cda374bf0fa728a75ad1f37415d8ec47ef0e0
Nicholas Arsenault <nickarsenault18@gmail.com>
2022-09-28 19:33:12 -0400

    Minor changes and added beginnings of AccountManager for Google logins.

--- App/app/src/main/java/com/example/app/Title_Page.java
+++ App/app/src/main/java/com/example/app/Title_Page.java
@@ -2,9 +2,12 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Title_Page extends AppCompatActivity {
 
@@ -14,13 +17,18 @@ public class Title_Page extends AppCompatActivity {
         setContentView(R.layout.activity_title_page);
     }
 // this gets a button press to move pages, add: android:onClick="moveToTitle" to xml file under the button's area, and add: tools:ignore="UsingOnClickInXml" to clear the error attached.
-    public void moveToHome(View view) {
+    public void moveToHome(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void moveToCreationPage(View view) {
+    public void moveToCreationPage(View v) {
         Intent intent = new Intent(this, Create_Account_Page.class);
         startActivity(intent);
+    }
+
+    public void login(View v) {
+        AccountManager acc = AccountManager.get(this);
+        Account[] accounts = acc.getAccountsByType("com.google");
     }
 }
