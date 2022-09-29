566f965d999759760d0815315ca84a04a51c5738
Blaine Oania <blaine.oania@gmail.com>
2022-09-29 09:41:42 -0400

    Solved crash using logcat stack trace. Expandable view now shows up, but it shows up extremely small and in the top right corner. Build is stable, needs fixing.

--- App/app/src/main/java/com/example/app/activity_test_page.java
+++ App/app/src/main/java/com/example/app/activity_test_page.java
@@ -2,8 +2,10 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -17,7 +19,11 @@ public class activity_test_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_page);
 
+        @IdRes int scrollViewID = View.generateViewId();
+
         StackExpandableView verticalStack = findViewById(R.id.verticalStack);
+        ScrollView scrollView = findViewById(R.id.verticalScrollView);
+        scrollView.setId(scrollViewID);
 
         ArrayList<TextView> viewList = new ArrayList<>();
 
@@ -28,7 +34,26 @@ public class activity_test_page extends AppCompatActivity {
 //            viewList.add(textView);
 //        }
 
-        verticalStack.setWidgets(viewList);
+//        verticalStack.setWidgets(viewList);
+
+        @IdRes int id = View.generateViewId();
+        @IdRes int id2 = View.generateViewId();
+        @IdRes int id3 = View.generateViewId();
+        final TextView textView = new TextView(this);
+        final TextView textView2 = new TextView(this);
+        final TextView textView3 = new TextView(this);
+        textView.setText("View");
+        textView.setId(id);
+        textView.setTextSize(30);
+        textView2.setText("View");
+        textView2.setId(id);
+        textView.setTextSize(30);
+        textView3.setText("View");
+        textView3.setId(id);
+        textView.setTextSize(30);
+        verticalStack.addWidget(textView);
+        verticalStack.addWidget(textView2);
+        verticalStack.addWidget(textView3);
 
     }
 
