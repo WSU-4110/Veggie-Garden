bd807592d504fed24188bf71a8180e60038ecbfe
Blaine Oania <blaine.oania@gmail.com>
2022-09-29 02:10:00 -0400

    Set up xml and java files to work with expandable view. To Do: Figure out how to create views and add them to the stack.

--- App/app/src/main/java/com/example/app/activity_test_page.java
+++ App/app/src/main/java/com/example/app/activity_test_page.java
@@ -1,9 +1,14 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import it.fabiosassu.stackexpandableview.StackExpandableView;
 
 public class activity_test_page extends AppCompatActivity {
 
@@ -12,13 +17,19 @@ public class activity_test_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_page);
 
-        final TextView[] viewList = new TextView[5];
+        StackExpandableView verticalStack = findViewById(R.id.verticalStack);
 
-        for (int i = 0; i < 5; i++) {
-            final TextView textView = new TextView(this);
-            textView.setText("View " + i);
-            viewList[i] = textView;
-        }
+        ArrayList<TextView> viewList = new ArrayList<>();
+
+//        for (int i = 0; i < 5; i++) {
+//            final TextView textView = new TextView(this);
+//
+//            textView.setText("View ");
+//            viewList.add(textView);
+//        }
+
+        verticalStack.setWidgets(viewList);
+
     }
 
 
