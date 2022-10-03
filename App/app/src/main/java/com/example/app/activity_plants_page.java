fbeab118957d3e3a485cbf83cae97a09b46d2833
Blaine Oania <blaine.oania@gmail.com>
2022-10-02 21:23:55 -0400

    Removed external dependancy, created plant page.

---
+++ App/app/src/main/java/com/example/app/activity_plants_page.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_plants_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plants_page);
+    }
+
+
+}
