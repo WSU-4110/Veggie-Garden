7e450296671caaedd11e61840fc99520395de40b
Blaine <Blaine.Oania@gmail.com>
2022-10-03 02:43:18 -0400

    New bug, this time app crashing because AddView is not valid for adapters.

--- App/app/src/main/java/com/example/app/activity_plants_page.java
+++ App/app/src/main/java/com/example/app/activity_plants_page.java
@@ -1,8 +1,15 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import java.util.List;
 
 public class activity_plants_page extends AppCompatActivity {
 
@@ -10,6 +17,22 @@ public class activity_plants_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plants_page);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        ConstraintLayout myLayout = (ConstraintLayout) findViewById(R.id.constraint_layout);
+        ConstraintSet myConstraints = new ConstraintSet();
+        myConstraints.clone(myLayout);
+
+        for (int i = 0; i < 5; i++) {
+            TextView newTextView = new TextView(this);
+            newTextView.setText("View " + i);
+            newTextView.setId(View.generateViewId());
+
+            myConstraints.connect(newTextView.getId(), ConstraintSet.RIGHT, R.id.list_view, ConstraintSet.RIGHT, 0);
+            myConstraints.connect(newTextView.getId(), ConstraintSet.TOP, R.id.list_view, ConstraintSet.TOP, 0);
+
+            listView.addView(newTextView);
+        }
     }
 
 
