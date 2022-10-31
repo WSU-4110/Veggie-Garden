34649894e5d5fe2bc5e91eb51fc723e9cf5607f8
Blaine <Blaine.Oania@gmail.com>
2022-10-31 12:34:46 -0400

    This has stackable text views in a linearlayout. We can also create 1 plantid box, however we still have the same issue where multiple plant views get stacked on top of each other

--- App/app/src/main/java/com/example/app/activity_plants_page.java
+++ App/app/src/main/java/com/example/app/activity_plants_page.java
@@ -25,28 +25,27 @@ public class activity_plants_page extends AppCompatActivity {
 
 
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
-        ConstraintLayout myLayout = (ConstraintLayout) findViewById(R.id.constraint_layout);
-        ConstraintSet myConstraints = new ConstraintSet();
-        myConstraints.clone(myLayout);
 
-        for (int i = 0; i < 1; i++) {
-//            TextView newTextView = new TextView(this);
-//            newTextView.setText("View " + i);
-//            newTextView.setId(View.generateViewId());
+//        ConstraintSet myConstraints = new ConstraintSet();
+//        myConstraints.clone(myLayout);
+
+        for (int i = 0; i < 5; i++) {
+            TextView newTextView = new TextView(this);
+            newTextView.setText("View " + i);
+            newTextView.setId(View.generateViewId());
 //
 //            myConstraints.connect(newTextView.getId(), ConstraintSet.RIGHT, R.id.linear_layout, ConstraintSet.RIGHT, 0);
 //            myConstraints.connect(newTextView.getId(), ConstraintSet.TOP, R.id.linear_layout, ConstraintSet.TOP, 0);
 //
-//            linearLayout.addView(newTextView);
+            linearLayout.addView(newTextView);
 
 
-            PlantId newPlant = new PlantId(this);
-            newPlant.setPlantName("Test Plant");
-            newPlant.setBackgroundColor(GREEN);
+//            PlantId newPlant = new PlantId(this);
+//            newPlant.setPlantName("Test Plant");
 //            newPlant.setTextColor(Color.valueOf(WHITE));
-            newPlant.setPlantBirthday("10/19/2022");
+//            newPlant.setPlantBirthday("10/19/2022");
 
-            linearLayout.addView(newPlant);
+//            linearLayout.addView(newPlant);
 
         }
     }
