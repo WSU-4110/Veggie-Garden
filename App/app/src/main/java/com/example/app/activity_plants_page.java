1bcde08e0eba03a79675662cbc75434eb650983c
Blaine <Blaine.Oania@gmail.com>
2022-10-31 16:13:16 -0400

    First workable version of PlantID. Custom view properly stacks in linearlayout. Need to work on prettifying and adding relevant information.

--- App/app/src/main/java/com/example/app/activity_plants_page.java
+++ App/app/src/main/java/com/example/app/activity_plants_page.java
@@ -24,28 +24,28 @@ public class activity_plants_page extends AppCompatActivity {
         setContentView(R.layout.activity_plants_page);
 
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
+        LinearLayout linearLayout = findViewById(R.id.linear_layout);
 
 //        ConstraintSet myConstraints = new ConstraintSet();
 //        myConstraints.clone(myLayout);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 2; i++) {
             TextView newTextView = new TextView(this);
-            newTextView.setText("View " + i);
-            newTextView.setId(View.generateViewId());
-//
-//            myConstraints.connect(newTextView.getId(), ConstraintSet.RIGHT, R.id.linear_layout, ConstraintSet.RIGHT, 0);
-//            myConstraints.connect(newTextView.getId(), ConstraintSet.TOP, R.id.linear_layout, ConstraintSet.TOP, 0);
-//
-            linearLayout.addView(newTextView);
+//            newTextView.setText("View " + i);
+//            newTextView.setId(View.generateViewId());
+////
+////            myConstraints.connect(newTextView.getId(), ConstraintSet.RIGHT, R.id.linear_layout, ConstraintSet.RIGHT, 0);
+////            myConstraints.connect(newTextView.getId(), ConstraintSet.TOP, R.id.linear_layout, ConstraintSet.TOP, 0);
+////
+//            linearLayout.addView(newTextView);
 
 
-//            PlantId newPlant = new PlantId(this);
-//            newPlant.setPlantName("Test Plant");
-//            newPlant.setTextColor(Color.valueOf(WHITE));
-//            newPlant.setPlantBirthday("10/19/2022");
+            PlantId newPlant = new PlantId(this);
+            newPlant.setPlantName("Test Plant");
+            newPlant.setTextColor(Color.valueOf(WHITE));
+            newPlant.setPlantBirthday("10/19/2022");
 
-//            linearLayout.addView(newPlant);
+            linearLayout.addView(newPlant);
 
         }
     }
