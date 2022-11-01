46e7faf1bc5dae1ec019203336525b9f890cc0c6
Tyler Groat <gh5295@wayne.edu>
2022-10-31 23:13:27 -0400

    New branch, adding virtual garden

---
+++ App/app/src/main/java/com/example/app/VirtualGarden.java
@@ -0,0 +1,16 @@
+package com.example.app;
+/*
+ * This class creates a virtual garden.
+ * VERSION 1
+ * 1. The user inputs how many planter areas of each veggie they have, for each of their
+ * veggie types.
+ * [hit edit virtual garden button] -> step 2.
+ * 2. The user drags and drops the veggie icons onto the virtual garden in order to make the
+ * arrangement that they chose.
+ * 3. [hit done button to finish editing the virtual garden] -> [disable the drag and drop feature]
+ * 4. -> [enable icon links in the virtual garden thant link them to their respective plantID page]
+ *
+ * */
+public class VirtualGarden {
+
+}
