79144f58fce9e5904c7d46f38980e992bb9b5875
Nicholas Arsenault <nickarsenault18@gmail.com>
2022-11-09 01:23:34 -0500

    Working version 1.3 with tons of fixes to latest big changes, new content to come soon.

--- App/app/src/main/java/com/example/app/WeatherActivity.java
+++ App/app/src/main/java/com/example/app/WeatherActivity.java
@@ -1,35 +1,25 @@
 package com.example.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.app.DownloadManager;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.text.DecimalFormat;
 
     public class WeatherActivity extends AppCompatActivity {
         EditText etCity, etCountry;
         TextView tvResult;
-        private final String url= "https://api.openweatehrmap.org/data/2.5/weather"; //Error with Url
-        private final String appid="94a4143658ad8966e26f35512cd0f006";
         DecimalFormat df = new DecimalFormat("#.##");
 
 
@@ -41,63 +31,57 @@ import java.text.DecimalFormat;
             etCity=findViewById(R.id.etCity);
             etCountry=findViewById(R.id.etCountry);
             tvResult=findViewById(R.id.tvResult); //Bug
-
         }
 
+        @SuppressLint("SetTextI18n")
         public void getWeatherDetails(View view) {
-            String tempUrl = "";
+            String tempUrl;
             String city = etCity.getText().toString().trim();
             String country = etCountry.getText().toString().trim();
             if(city.equals("")){
                 tvResult.setText("City field can not be empty!");
             }else{
+                //Error with Url
+                String url = "https://api.openweatehrmap.org/data/2.5/weather";
+                String appID = "94a4143658ad8966e26f35512cd0f006";
                 if(!country.equals("")){
-                    tempUrl = url + "?q=" + city + "," + country + "&appid=" + appid;
+                    tempUrl = url + "?q=" + city + "," + country + "&appID=" + appID;
                 }else{
-                    tempUrl = url + "?q=" + city + "&appid=" + appid;
+                    tempUrl = url + "?q=" + city + "&appID=" + appID;
                 }
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        String output = "";
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            JSONArray jsonArray = jsonResponse.getJSONArray("weather");
-                            JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
-                            String description = jsonObjectWeather.getString("description");
-                            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
-                            double temp = jsonObjectMain.getDouble("temp") - 273.15;
-                            double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
-                            float pressure = jsonObjectMain.getInt("pressure");
-                            int humidity = jsonObjectMain.getInt("humidity");
-                            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
-                            String wind = jsonObjectWind.getString("speed");
-                            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
-                            String clouds = jsonObjectClouds.getString("all");
-                            JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
-                            String countryName = jsonObjectSys.getString("country");
-                            String cityName = jsonResponse.getString("name");
-                            tvResult.setTextColor(Color.rgb(68,134,199));
-                            output += "Current weather of " + cityName + " (" + countryName + ")"
-                                    + "\n Temp: " + df.format(temp) + " °C"
-                                    + "\n Feels Like: " + df.format(feelsLike) + " °C"
-                                    + "\n Humidity: " + humidity + "%"
-                                    + "\n Description: " + description
-                                    + "\n Wind Speed: " + wind + "m/s (meters per second)"
-                                    + "\n Cloudiness: " + clouds + "%"
-                                    + "\n Pressure: " + pressure + " hPa";
-                            tvResult.setText(output);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, response -> {
+                    String output = "";
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        String wind = jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        String clouds = jsonObjectClouds.getString("all");
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        String countryName = jsonObjectSys.getString("country");
+                        String cityName = jsonResponse.getString("name");
+                        tvResult.setTextColor(Color.rgb(68,134,199));
+                        output += "Current weather of " + cityName + " (" + countryName + ")"
+                                + "\n Temp: " + df.format(temp) + " °C"
+                                + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                                + "\n Humidity: " + humidity + "%"
+                                + "\n Description: " + description
+                                + "\n Wind Speed: " + wind + "m/s (meters per second)"
+                                + "\n Cloudiness: " + clouds + "%"
+                                + "\n Pressure: " + pressure + " hPa";
+                        tvResult.setText(output);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                }, new Response.ErrorListener(){
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+                }, error -> Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show());
                 RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
                 requestQueue.add(stringRequest);
             }
--- App/app/src/main/java/com/example/app/activity_plants_page.java
+++ App/app/src/main/java/com/example/app/activity_plants_page.java
@@ -1,23 +1,19 @@
 package com.example.app;
 
-import static android.graphics.Color.GREEN;
 import static android.graphics.Color.WHITE;
-
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
-
-import com.example.app.PlantId;
 
 public class activity_plants_page extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,19 +22,19 @@ public class activity_plants_page extends AppCompatActivity {
 
         LinearLayout linearLayout = findViewById(R.id.linear_layout);
 
-//        ConstraintSet myConstraints = new ConstraintSet();
-//        myConstraints.clone(myLayout);
+/**        ConstraintSet myConstraints = new ConstraintSet();
+ myConstraints.clone(myLayout);
 
-        for (int i = 0; i < 8; i++) {
-            TextView newTextView = new TextView(this);
-//            newTextView.setText("View " + i);
-//            newTextView.setId(View.generateViewId());
-////
-////            myConstraints.connect(newTextView.getId(), ConstraintSet.RIGHT, R.id.linear_layout, ConstraintSet.RIGHT, 0);
-////            myConstraints.connect(newTextView.getId(), ConstraintSet.TOP, R.id.linear_layout, ConstraintSet.TOP, 0);
-////
-//            linearLayout.addView(newTextView);
+ for (int i = 0; i < 8; i++) {
+ TextView newTextView = new TextView(this);
+ newTextView.setText("View " + i);
+ newTextView.setId(View.generateViewId());
 
+ myConstraints.connect(newTextView.getId(), ConstraintSet.RIGHT, R.id.linear_layout, ConstraintSet.RIGHT, 0);
+ myConstraints.connect(newTextView.getId(), ConstraintSet.TOP, R.id.linear_layout, ConstraintSet.TOP, 0);
+
+ linearLayout.addView(newTextView);}
+ **/
 
             PlantId newPlant = new PlantId(this);
             newPlant.setPlantName("Test Plant");
@@ -51,4 +47,4 @@ public class activity_plants_page extends AppCompatActivity {
     }
 
 
-}
+
