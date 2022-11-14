84a36aad9501695765ae30adc032021625baa4a8
Nicholas Arsenault <nickarsenault18@gmail.com>
2022-11-14 18:59:02 -0500

    UnitTest created

---
+++ App/app/src/test/java/com/example/app/CredentialsTest.java
@@ -0,0 +1,20 @@
+package com.example.app;
+
+class CredentialsTest {
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPassword() {
+    }
+}
