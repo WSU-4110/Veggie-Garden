c4a577063d561297853fa253786bb55ae0569803
Nicholas Arsenault <nickarsenault18@gmail.com>
2022-11-14 19:12:20 -0500

    UnitTest created with ChangePasswordTest, and CreateAccountPageTest. All working and passed using JUnit5 auto-integrated software. Nicholas Arsenault

---
+++ App/app/src/test/java/com/example/app/ChangePasswordTest.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChangePasswordTest {
+
+    @Test
+    void onCreate() {
+    }
+}
